9da411337d0be43d027b050bf593b60350353d40
Paul Kinlan <paul.kinlan@gmail.com>
2018-05-20 23:53:34 +0100

    SW should not be absolute path

--- index.js
+++ index.js
@@ -238,7 +238,7 @@ function measureNaturalPreWidth(pre) {
 function installServiceWorker() {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register('/sw.js').then(registration => {
+      navigator.serviceWorker.register('sw.js').then(registration => {
         console.log('SW registered: ', registration);
       }).catch(registrationError => {
         console.log('SW registration failed: ', registrationError);
