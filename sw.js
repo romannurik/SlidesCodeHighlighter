f366d8943487da24c62e927ef512c60a2d17e462
romannurik <roman@nurik.net>
2022-05-14 03:13:57 +0000

    Deploying to gh-pages from @ romannurik/SlidesCodeHighlighter@c8e48908841f4b43b38bdc7a5dde71f6811ada2a 🚀

--- sw.js
+++ sw.js
@@ -23,48 +23,20 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "ace.js",
-    "revision": "e1ec2dda9ad25e3e4005b01372210e84"
-  },
-  {
     "url": "index.css",
-    "revision": "7f033b9aa8a76e42318801efb3642f10"
+    "revision": "e14065bca6a679002054f0e7392010e0"
   },
   {
     "url": "index.html",
-    "revision": "05ac40a1deabb3735c49f73908745795"
+    "revision": "13a534c56dd58c639ed1aab11f0bb62c"
   },
   {
     "url": "index.js",
-    "revision": "e2959cd57a762ac9679c36c79e364a32"
-  },
-  {
-    "url": "jquery.min.js",
-    "revision": "12108007906290015100837a6a61e9f4"
-  },
-  {
-    "url": "material-colors.js",
-    "revision": "62d4d679092da70d66e51aa2752aa17f"
-  },
-  {
-    "url": "mode-text.js",
-    "revision": "c01becee0a5e9e847c9dd4a789761925"
+    "revision": "e10dbe0d331f0b38fcdb29659c48a73d"
   },
   {
     "url": "prism.js",
     "revision": "9d460c42bfefb4ded3fedb6a81a3b987"
-  },
-  {
-    "url": "theme-chrome.js",
-    "revision": "52a070e8c00b6a9ef1137c1d7f9ecd97"
-  },
-  {
-    "url": "themes.js",
-    "revision": "7735217178f12bc2bf9637816e5fc176"
-  },
-  {
-    "url": "webfontloader.js",
-    "revision": "7e2893ef542a12a5c7207d438bfa87d8"
   }
 ].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
